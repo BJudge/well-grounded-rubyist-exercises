e85b6092e661c2f0cdebab8efce694702f6847d2
BartJudge <BartJudge@gmail.com>
2018-12-04 04:55:29 +0000

    string interpolation

--- ticket.rb
+++ ticket.rb
@@ -26,13 +26,17 @@ class Ticket
 end
 
 ticket = Ticket.new
-  print "This ticket is for: "
-  print ticket.event + ", at "
-  print ticket.venue + ", on "
-  puts ticket.date + "."
-  print "The performer is "
-  puts ticket.performer + "."
-  print "the seat is "
-  print ticket.seat + ", "
-  print "and it costs $"
-  puts "%.2f. " % ticket.price
+  #print "This ticket is for: "
+  #print ticket.event + ", at "
+  #print ticket.venue + ", on "
+  #puts ticket.date + "."
+  #print "The performer is "
+  #puts ticket.performer + "."
+  #print "the seat is "
+  #print ticket.seat + ", "
+  #print "and it costs $"
+  #puts "%.2f. " % ticket.price
+  puts "This ticket is for: #{ticket.event}, at #{ticket.venue}. " +
+  "The performer is #{ticket.performer}. " +
+  "The seat is #{ticket.seat}, " +
+  "and it costs $#{"%.2f." % ticket.price}"
